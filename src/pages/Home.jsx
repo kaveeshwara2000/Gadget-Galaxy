16cb617dec337b72e39c704ace96be64df7f86e4
kaveeshwara2000 <sachindukaveeshwara@gmail.com>
2024-06-16 22:52:24 +0530

    project creating and setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import Main from '../components/Main'
+
+
+export function Home() {
+  return (
+    <div>
+      <Sidebar/>
+      <div>
+        <Main/>
+      </div>
+    </div>
+  )
+}
